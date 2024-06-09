500212f46c38ea2bb760a08cec6a0bb94945f01f
bhaveshcodespace <bhaveshvagadiya1990@gmail.com>
2024-06-09 19:04:17 +0530

    added pkgs and templetes

---
+++ config.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Configuration variables
+WORKSPACE_NAME="marker_display_ws"
+ROS_DISTRO="galactic"
+WORKSPACE_PATH="$PWD/${WORKSPACE_NAME}"
+PACKAGE_NAME="marker_display"
+DESTINATION_DIR="${WORKSPACE_PATH}/src"
